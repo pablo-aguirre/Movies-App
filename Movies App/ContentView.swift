41ac3e3e94a9193444719f5cf4701501e9c6b976
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-16 17:20:31 +0200

    Testing HTTP Client

--- Movies App/ContentView.swift
+++ Movies App/ContentView.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello World!")
+            .onAppear {
+                HTTPClient().getMoviewBy(search: "batman") { result in
+                    switch result {
+                    case .success(let movies):
+                        print(movies)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            }
     }
 }
 
