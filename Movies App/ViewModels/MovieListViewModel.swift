6b5f33d20fcfb50a557eddabccfda1340c732433
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-16 17:42:29 +0200

    Movie List ViewModel

---
+++ Movies App/ViewModels/MovieListViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  MovieListViewModel.swift
+//  Movies App
+//
+//  Created by Pablo Aguirre on 16/07/24.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class MovieListViewModel {
+    var movies = [MovieViewModel]()
+    private let httpClient = HTTPClient()
+    
+    func searchByName(_ name: String) {
+        httpClient.getMoviewBy(search: name) { result in
+            switch result {
+            case .success(let movies):
+                if let movies = movies {
+                    DispatchQueue.main.async {
+                        self.movies = movies.map{ MovieViewModel.init(movie: $0) }
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+struct MovieViewModel {
+    let movie: Movie
+    
+    var imdbId: String {
+        movie.imdbID
+    }
+    
+    var title: String {
+        movie.title
+    }
+    
+    var poster: String {
+        movie.title
+    }
+    
+    var year: String {
+        movie.year
+    }
+}
