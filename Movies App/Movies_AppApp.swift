a948953a7d409c67abec94aca90926dee120d905
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-16 15:52:21 +0200

    Initial Commit

---
+++ Movies App/Movies_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_AppApp.swift
+//  Movies App
+//
+//  Created by Pablo Aguirre on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Movies_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
